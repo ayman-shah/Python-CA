6e73e079c1b59fdb410527f9c73b1ac3bd23879e
ayman-shah <65572052+ayman-shah@users.noreply.github.com>
2020-07-21 10:29:26 +1200

    Add files via upload

---
+++ Python 1/7.3.py
@@ -0,0 +1,15 @@
+# Set up price variables
+ps3_game = 20
+ps4_game = 45
+
+#Order #1: 1 PS3 game and 2 PS4 games
+order1_price = ps3_game + ps4_game * 2
+
+#Order #2: 4 PS3 games, 3 PS4 games
+order2_price = ps3_game * 4 + ps4_game * 3
+
+
+
+#Print out total order costs
+print("Order 1 costs: ${}".format(order1_price))
+print("Order 2 costs: ${}".format(order2_price))
